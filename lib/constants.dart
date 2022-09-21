bda99712eea28494fef9ca2d337bb0a7dfb4603e
Châu Hoàng Khiêm <91679167+khiemhg1202@users.noreply.github.com>
2022-09-21 14:30:50 +0700

    Delete constants.dart

--- lib/constants.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-const kPrimaryColor = Color(0xFFF9F9F9);
-const kTextColor = Color(0xFF150B3D);
-const kTextColorS = Color(0xFF524B6B);
-const kBackgroundColor = Color(0xFFFFFFFF);
-
